68307fc6d7b3571fecdb37b55a9bf5cdedd0c7d4
Spodai <driesvanheverlee@gmail.com>
2025-04-11 14:59:00 +0200

    Update team_banners_lite.py

--- team_banners_lite.py
+++ team_banners_lite.py
@@ -1,14 +1,3 @@
-# all writen with gpt, but hey it works :D
-# rundown what it does:
-## downloads all files from a public google folder
-## puts them in an import folder
-## slaps that data into a csv which looks like: DATE | ORIGINAL FILE NAME | NEW FILE NAME | S-UL.EU URL
-## asks for user imput to rename files that are not listed yet in the csv
-## copies those renamed files to an export folder
-## uploads the files that were renamed to s-ul.eu
-## fills on the csv with the renames and urls
-
-
 import os
 import shutil
 import requests
